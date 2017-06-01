3a4c389bb9e9ccafd97ee842d456b48277b02dbb
Felix C.Courtemanche <f@turbulent.ca>
2017-06-01 10:25:25 -0400

    updated run-redis to use su

--- run-redis.sh
+++ run-redis.sh
@@ -4,5 +4,5 @@
 /systpl/systpl.jinja.py /systpl/make-backup.sh.tmpl > /make-backup.sh
 chmod +x /make-backup.sh
 
-mkdir -p /vol/database/`hostname`-redis && chown heap:root /vol/database/`hostname`-redis
-exec sudo -u heap -H /usr/bin/redis-server /etc/redis/redis.conf
+mkdir -p /vol/database/`hostname`-redis && chown heap:root /etc/redis/redis.conf /vol/database/`hostname`-redis 
+exec su heap -c "/usr/bin/redis-server /etc/redis/redis.conf"
